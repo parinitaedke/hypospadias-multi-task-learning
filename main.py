0a4ffcad7def568e19ae5ecdca11bb75bad1b4a8
Parinita Edke <parinita.edke@mail.utoronto.ca>
2023-08-04 12:38:12 -0400

    committing code for vanilla multitask trainer

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from utils.utils import get_transformations, build_model, setup_dataloader, setup_criterion
 from vanilla_singletask_model.trainer import Trainer as VSTMT
+from vanilla_multitask_model.trainer import Trainer as VMTMT
 from torch.optim.lr_scheduler import ExponentialLR
 
 # set up random seed for reproducibility
@@ -24,11 +25,11 @@ def run_experiment():
         'batch_size': 2,
         'num_workers': 0,  # anything > 0 is giving issues -- why?
         'pin_memory': True,
-        'num_epochs': 1,
+        'num_epochs': 40,
         'loss': 'SteeperMSE',     # 'MSE, 'SteeperMSE', 'CE', 'MT-indep-seghead-loss', 'MT-new-seghead-loss'
         'steeper_MSE_coeff': 10,
         'model_type': 'vanilla-singletask',
-        'model_name': 'resnet50',   # 'swin'/'vit' + '_'  + 'small', 'tiny', 'base' || 'resnet' + '18'/'50'
+        'model_name': 'vit_base',   # 'swin'/'vit' + '_'  + 'small', 'tiny', 'base' || 'resnet' + '18'/'50'
         'lr': 0.01,
         'weight_decay': 0.001,
         'gamma': 0.85,
@@ -49,25 +50,36 @@ def run_experiment():
     # setup model
     models = build_model(wandb.config)
 
-    for bodypart in wandb.config['anatomy_part']:
-        models[bodypart] = models[bodypart].to(wandb.config['device'])
-
     # create train dataset and dataloaders
     train_loader, valid_loader, test_loader = setup_dataloader(wandb.config, data_transforms)
 
     # set up loss function
     criterion = setup_criterion(wandb.config)
 
-    # set up optimizer and scheduler
-    optimizers, schedulers = {}, {}
-    for bodypart in wandb.config['anatomy_part']:
-        optimizers[bodypart] = torch.optim.Adam(params=models[bodypart].parameters(), lr=wandb.config['lr'],
-                                    weight_decay=wandb.config['weight_decay'])
-        schedulers[bodypart] = ExponentialLR(optimizers[bodypart], gamma=wandb.config['gamma'])
-
     # create and run trainer
     if wandb.config['model_type'].startswith('vanilla-singletask'):
+
+        # set up optimizer and scheduler and put models on device
+        optimizers, schedulers = {}, {}
+        for bodypart in wandb.config['anatomy_part']:
+            models[bodypart] = models[bodypart].to(wandb.config['device'])
+            optimizers[bodypart] = torch.optim.Adam(params=models[bodypart].parameters(), lr=wandb.config['lr'],
+                                                    weight_decay=wandb.config['weight_decay'])
+            schedulers[bodypart] = ExponentialLR(optimizers[bodypart], gamma=wandb.config['gamma'])
+
+
         VSTMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers, schedulers=schedulers).run(
+            train_loader, valid_loader
+        )
+    elif wandb.config['model_type'].startswith('vanilla-multitask'):
+        models = models.to(wandb.config['device'])
+
+        optimizers = torch.optim.Adam(params=models.parameters(), lr=wandb.config['lr'],
+                                                weight_decay=wandb.config['weight_decay'])
+        schedulers = ExponentialLR(optimizers, gamma=wandb.config['gamma'])
+
+        VMTMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers,
+              schedulers=schedulers).run(
             train_loader, valid_loader
         )
 
