60ce9bccc9d1b434596703e494da4292558c630f
Parinita Edke <parinita.edke@mail.utoronto.ca>
2023-09-07 12:14:51 -0400

    modified code for additional loaders, loss functions, augmentations
    The dataset loader code is modified to allow for additional training datasets. The loss function code is updated for additional
    dice loss function variations.

--- utils/utils.py
+++ utils/utils.py
@@ -9,7 +9,7 @@ import albumentations as A
 import albumentations.pytorch as Apy
 
 # from loss import SteeperMSELoss, DiceLoss, InfluenceSegmentationLoss
-from .loss import SteeperMSELoss, DiceLoss, InfluenceSegmentationLoss
+from .loss import SteeperMSELoss, DiceLoss, WeightedSoftDiceLoss, InfluenceSegmentationLoss
 from .dataset import HypospadiasDataset
 
 from vanilla_multitask_model.model import Vanilla_Multitask_Model
@@ -26,8 +26,9 @@ def load_checkpoint(checkpoint, model):
 def get_transformations():
     data_transforms = {
         'train': A.Compose([
-            A.PadIfNeeded(min_height=224, min_width=224),
-            A.RandomCrop(224, 224),
+            # A.PadIfNeeded(min_height=224, min_width=224),
+            # A.RandomCrop(224, 224),
+            A.Resize(224, 224),
             A.Rotate(limit=30),
             # A.RandomResizedCrop(224, 224, ratio=(1.0, 1.0), scale=(0.9, 1.0)),
             A.HorizontalFlip(),
@@ -35,8 +36,9 @@ def get_transformations():
             Apy.transforms.ToTensorV2()
         ]),
         'valid': A.Compose([
-            A.Resize(256, 256),
-            A.CenterCrop(224, 224),
+            A.Resize(224, 224),
+            # A.Resize(256, 256),
+            # A.CenterCrop(224, 224),
             A.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             A.pytorch.transforms.ToTensorV2()
         ]),
@@ -88,6 +90,16 @@ def setup_dataloader(config, transforms):
 
     train_ds = HypospadiasDataset(config=config, score_df=score_df, preprocessing=transforms['train'], dataset_type='train')
     train_loader = DataLoader(train_ds, batch_size=config["batch_size"], num_workers=config["num_workers"], pin_memory=config["pin_memory"], shuffle=True)
+    
+    extra_train_ds_loaders = []
+    if any(config["extra_train_datasets"]):
+        for extra_train_ds_name in config["extra_train_datasets"]:
+            extra_train_ds = HypospadiasDataset(config=config, score_df=score_df, preprocessing=transforms['train'], dataset_type=extra_train_ds_name, include_masks=False)
+            extra_train_loader = DataLoader(extra_train_ds, batch_size=config["batch_size"], num_workers=config["num_workers"], pin_memory=config["pin_memory"], shuffle=True)
+            
+            extra_train_ds_loaders.append(extra_train_loader)
+            
+        
 
     val_ds = HypospadiasDataset(config=config, score_df=score_df, preprocessing=transforms['valid'], dataset_type='val')
     val_loader = DataLoader(val_ds, batch_size=config["batch_size"], num_workers=config["num_workers"], pin_memory=config["pin_memory"], shuffle=True)
@@ -95,7 +107,7 @@ def setup_dataloader(config, transforms):
     test_ds = HypospadiasDataset(config=config, score_df=score_df, preprocessing=transforms['valid'], dataset_type='test')
     test_loader = DataLoader(test_ds, batch_size=config["batch_size"], num_workers=config["num_workers"], pin_memory=config["pin_memory"], shuffle=True)
 
-    return train_loader, val_loader, test_loader
+    return train_loader, val_loader, test_loader, extra_train_ds_loaders
 
 def setup_criterion(config):
     if config['loss'] == 'MSE':
@@ -108,11 +120,17 @@ def setup_criterion(config):
     elif config['loss'] == 'CE':
         criterion = nn.CrossEntropyLoss() 
 
-    elif config['loss'] == 'MT-indep-seghead-loss':
+    elif config['loss'] == 'MT-dice-seghead-loss':
         # criterion1 = BCEWithLogitsLoss()
         criterion1 = DiceLoss()
         criterion2 = SteeperMSELoss(coefficient=config['steeper_MSE_coeff'])
 
+        criterion = [criterion1, criterion2]
+    
+    elif config['loss'] == 'MT-WSdice-seghead-loss':
+        criterion1 = WeightedSoftDiceLoss(v1=config['weighted_soft_dice_v1'])
+        criterion2 = SteeperMSELoss(coefficient=config['steeper_MSE_coeff'])
+        
         criterion = [criterion1, criterion2]
 
     elif config['loss'] == 'MT-new-seghead-loss':
