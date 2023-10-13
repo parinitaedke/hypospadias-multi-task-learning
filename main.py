be6d1c67ce1deee35fae9b4de5b1ed24ec801c5d
Parinita Edke <parinita.edke@mail.utoronto.ca>
2023-10-13 11:13:58 -0400

    reorganized the wandb config + modified code to allow for extra train ds

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ random.seed(2023)
 np.random.seed(2023)
 os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
 torch.use_deterministic_algorithms(True)
+torch.autograd.set_detect_anomaly(True)
 
 def run_experiment():
         # set up augmentation
@@ -32,26 +33,40 @@ def run_experiment():
         'batch_size': 2,
         'num_workers': 0,  # anything > 0 is giving issues -- why?
         'pin_memory': True,
-        'num_epochs': 1,
-        'loss': 'MT-indep-seghead-loss',     # 'MSE, 'SteeperMSE', 'CE', 'MT-indep-seghead-loss', 'MT-new-seghead-loss'
+        'num_epochs': 40,
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        'loss': 'MT-dice-overlap_penalty-seghead-loss',     # 'MSE, 'SteeperMSE', 'CE', 'MT-dice-seghead-loss','MT-WSdice-seghead-loss', 'MT-dice-overlap_penalty-seghead-loss'
         'steeper_MSE_coeff': 10,
-        'model_type': 'multitask_UNET_segmentation',
+        'weighted_soft_dice_v1': 0.1,
+        'overlap_loss_weight': 10,
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # Model specifications
+        'model_type': 'multitask_UNET_segmentation', # 'multitask_UNET_segmentation', 'vanilla-singletask'
         'model_name': 'vit_base',   # 'swin'/'vit' + '_'  + 'small', 'tiny', 'base' || 'resnet' + '18'/'50'
         'lr': 0.01,
         'weight_decay': 0.001,
         'gamma': 0.85,
-        'num_classes': [1, 1, 1],           # 4/5
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # This is for the singletask model
+        'num_classes': [1, 1, 1],           # 4/5; 
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # Dataset specifications
         'anatomy_part': ['Glans', 'Meatus', 'Shaft'],
+        'overlap_bodyparts': ['Foreskin'], # ['Foreskin', 'Fingers'],
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
+        # UNet model specifications
         'in_channels': 3,
         'out_channels': 3,
         'UNET features': [64, 128, 256, 512],
+        #-------------------------------------------------------------------------------------------------------------------------------------------------------------
         'freeze': False,
         'freeze_until': 'layer3',
         'device': torch.device("cuda" if torch.cuda.is_available() else "cpu"),     # 'cuda'
         'pretrained': True,
         'data_transforms': data_transforms,
-        'data_path': r"C:\Users\Parinita Edke\Desktop\Hypospadias-Project\Hypospadias-Data\datasets\19-April-23_split - no_special_chars",# '/home/mrizhko/hn_miccai/data/', 
-        'csv_path': r"C:\Users\Parinita Edke\Desktop\Hypospadias-Project\Hypospadias-Data\datasheets\FINAL Copy of Classification - no_special_chars.xlsx",
+        'data_path': "/home/pedke/dataset/datasets/30-August-23_split - no_special_chars", 
+        'csv_path': "/home/pedke/dataset/datasheets/FINAL Copy of Classification - no_special_chars.xlsx",
+        'extra_train_datasets': ['train-2'],
         'save_weights': True,
         'debug': False
     })
@@ -61,7 +76,7 @@ def run_experiment():
     models = build_model(wandb.config)
 
     # create train dataset and dataloaders
-    train_loader, valid_loader, test_loader = setup_dataloader(wandb.config, data_transforms)
+    train_loader, valid_loader, test_loader, extra_train_ds_loaders = setup_dataloader(wandb.config, data_transforms)
 
     # set up loss function
     criterion = setup_criterion(wandb.config)
@@ -79,7 +94,7 @@ def run_experiment():
 
 
         VSTMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers, schedulers=schedulers).run(
-            train_loader, valid_loader
+            train_loader, valid_loader, extra_train_ds_loaders
         )
     elif wandb.config['model_type'].startswith('vanilla-multitask'):
         models = models.to(wandb.config['device'])
@@ -88,28 +103,25 @@ def run_experiment():
                                                 weight_decay=wandb.config['weight_decay'])
         schedulers = ExponentialLR(optimizers, gamma=wandb.config['gamma'])
 
-        VMTMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers,
-              schedulers=schedulers).run(
-            train_loader, valid_loader
+        VMTMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers,schedulers=schedulers).run(
+            train_loader, valid_loader, extra_train_ds_loaders
         )
 
     elif wandb.config['model_type'].startswith('multitask_UNET_segmentation'):
         models = models.to(wandb.config['device'])
 
-        optimizers = torch.optim.Adam(params=models.parameters(), lr=wandb.config['lr'],
-                                      weight_decay=wandb.config['weight_decay'])
+        optimizers = torch.optim.Adam(params=models.parameters(), lr=wandb.config['lr'], weight_decay=wandb.config['weight_decay'])
         schedulers = ExponentialLR(optimizers, gamma=wandb.config['gamma'])
 
-        MTUNetSegMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers,
-              schedulers=schedulers).run(
-            train_loader, valid_loader
+        MTUNetSegMT(config=wandb.config, models=models, criterion=criterion, optimizers=optimizers, schedulers=schedulers).run(
+            train_loader, valid_loader, extra_train_ds_loaders
         )
 
 
 if __name__ == '__main__':
 
     # init wandb
-    wandb.init(project="multitask-UNET-segmentation-model")
+    wandb.init(project="dumb") # "new-loss-summation"
 
     # run experiment
     run_experiment()
