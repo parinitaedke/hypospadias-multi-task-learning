7d800902b6af54818eebbbbd917da806a3d192d3
Parinita Edke <parinita.edke@mail.utoronto.ca>
2023-10-25 14:12:56 -0400

    added HOPE heads

--- vanilla_multitask_model/model.py
+++ vanilla_multitask_model/model.py
@@ -8,6 +8,10 @@ class Vanilla_Multitask_Model(nn.Module):
         super().__init__()
         
         self.config = config
+        
+        if not self.config['hope_classification_heads'] and not self.config['gms_classification_heads']:
+            print('One set of scores has to be on')
+            exit(1)
 
         if self.config['model_name'].startswith('vit'):
             model_name = f"{self.config['model_name']}_patch16_224"
@@ -23,40 +27,41 @@ class Vanilla_Multitask_Model(nn.Module):
         if self.config['freeze']:
             submodules = [n for n, _ in self.model.named_children()]
             timm.freeze(self.model, submodules[:submodules.index(self.config['freeze_until']) + 1])
+            
+        if self.config['gms_classification_heads']:
+            # G head
+            self.g_head = nn.Sequential(
+                nn.Linear(in_features=1000, out_features=512),
+                nn.ReLU(),
+                nn.Linear(in_features=512, out_features=256),
+                nn.ReLU(),
+                nn.Linear(in_features=256, out_features=64),
+                nn.ReLU(),
+                nn.Linear(in_features=64, out_features=1)
+            )
+            # M head
+            self.m_head = nn.Sequential(
+                nn.Linear(in_features=1000, out_features=512),
+                nn.ReLU(),
+                nn.Linear(in_features=512, out_features=256),
+                nn.ReLU(),
+                nn.Linear(in_features=256, out_features=64),
+                nn.ReLU(),
+                nn.Linear(in_features=64, out_features=1)
+            )
 
-        # G head
-        self.g_head = nn.Sequential(
-            nn.Linear(in_features=1000, out_features=512),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Linear(in_features=256, out_features=64),
-            nn.ReLU(),
-            nn.Linear(in_features=64, out_features=1)
-        )
-        # M head
-        self.m_head = nn.Sequential(
-            nn.Linear(in_features=1000, out_features=512),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Linear(in_features=256, out_features=64),
-            nn.ReLU(),
-            nn.Linear(in_features=64, out_features=1)
-        )
-
-        # S head
-        self.s_head = nn.Sequential(
-            nn.Linear(in_features=1000, out_features=512),
-            nn.ReLU(),
-            nn.Linear(in_features=512, out_features=256),
-            nn.ReLU(),
-            nn.Linear(in_features=256, out_features=64),
-            nn.ReLU(),
-            nn.Linear(in_features=64, out_features=1)
-        )
+            # S head
+            self.s_head = nn.Sequential(
+                nn.Linear(in_features=1000, out_features=512),
+                nn.ReLU(),
+                nn.Linear(in_features=512, out_features=256),
+                nn.ReLU(),
+                nn.Linear(in_features=256, out_features=64),
+                nn.ReLU(),
+                nn.Linear(in_features=64, out_features=1)
+            )
         
-        if self.config['hope_classfication_heads']:
+        if self.config['hope_classification_heads']:
             
             # Meatus position head
             self.meatus_pos_head = nn.Sequential(
@@ -116,19 +121,23 @@ class Vanilla_Multitask_Model(nn.Module):
     def forward(self, x):
         interim_output = self.model(x)
 
-        g_score = self.g_head(interim_output)
-        m_score = self.m_head(interim_output)
-        s_score = self.s_head(interim_output)
+        if self.config['gms_classification_heads']:
+            g_score = self.g_head(interim_output)
+            m_score = self.m_head(interim_output)
+            s_score = self.s_head(interim_output)
         
-        if self.config['hope_classfication_heads']:
+        if self.config['hope_classification_heads']:
             meatus_pos_score = self.meatus_pos_head(interim_output)
             meatus_shape_score = self.meatus_shape_head(interim_output)
             glans_shape_score = self.glans_shape_head(interim_output)
             penile_skin_shape_score = self.penile_skin_shape_head(interim_output)
             torsion_score = self.torsion_head(interim_output)
             
-
-        if self.config['hope_classifcation_heads']:
-            return (g_score, m_score, s_score), (meatus_pos_score, meatus_shape_score, glans_shape_score, penile_skin_shape_score, torsion_score)
+        if self.config['hope_classification_heads'] and not self.config['gms_classification_heads']:
+            return (meatus_pos_score, meatus_shape_score, glans_shape_score, penile_skin_shape_score, torsion_score)
+        
+        elif not self.config['hope_classification_heads'] and self.config['gms_classification_heads']:
+            return (g_score, m_score, s_score)
+        
         else:
-            return g_score, m_score, s_score
+            return (g_score, m_score, s_score), (meatus_pos_score, meatus_shape_score, glans_shape_score, penile_skin_shape_score, torsion_score)
